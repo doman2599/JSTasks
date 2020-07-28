1a21577513eb76db6be6d574d4fe4309c255b384
doman2599 <68905548+doman2599@users.noreply.github.com>
2020-07-28 15:48:06 +0300

    Update FirstTask.js

--- FirstTask.js
+++ FirstTask.js
@@ -1,13 +1,12 @@
 var arr = [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]];
 var sortingarr = [];
-for(let i = 0; i < arr.length; i++){
-  for (let j = 0; j < arr[i].length; j++){
-    sortingarr.push(arr[i][j]);
-  }
-}
+
+var flattened = arr.reduce(function(a, b) {
+  return a.concat(b);
+});  
+
+console.log(flattened.sort(compareNumbers).reverse())
 
 function compareNumbers(a, b) {
   return a - b;
 }
-
-console.log(sortingarr.sort(compareNumbers).reverse());
